3c7786307d2b960642a1abf75e9e0fa444f5f0c7
MohamedHalim <mohamedhalim1998@gmail.com>
2020-07-17 06:11:19 +0200

    add animation

--- app/src/main/java/com/mohamed/halim/essa/flashcards/cardscreen/CardAdapter.kt
+++ app/src/main/java/com/mohamed/halim/essa/flashcards/cardscreen/CardAdapter.kt
@@ -1,14 +1,18 @@
 package com.mohamed.halim.essa.flashcards.cardscreen
 
+import android.animation.*
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.mohamed.halim.essa.flashcards.data.model.Card
 import com.mohamed.halim.essa.flashcards.databinding.CardListItemBinding
 
-class CardAdapter : ListAdapter<Card, CardViewHolder>(CardDiffCallBacks()) {
+class CardAdapter() :
+    ListAdapter<Card, CardViewHolder>(CardDiffCallBacks()) {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CardViewHolder {
         return CardViewHolder.from(parent)
     }
@@ -37,7 +41,36 @@ class CardViewHolder private constructor(val binding: CardListItemBinding) :
     ) {
         binding.executePendingBindings()
         binding.card = card
+        binding.root.setOnClickListener {
+            animate(card, it, binding.cardText)
+        }
     }
+
+    private fun animate(card: Card, view: View, cardText: TextView) {
+        ObjectAnimator.ofFloat(view, View.ROTATION_Y, -180f, 0f).apply {
+            repeatMode = ObjectAnimator.REVERSE
+            cardText.visibility = View.INVISIBLE
+            addListener(object : AnimatorListenerAdapter() {
+                override fun onAnimationEnd(animation: Animator?) {
+                    changeText(card)
+                    cardText.visibility = View.VISIBLE
+                }
+            })
+            start()
+        }
+
+    }
+
+    private fun changeText(card: Card) {
+        binding.cardText.apply {
+            text = if (text == card.firstSide) {
+                card.secondSide
+            } else {
+                card.firstSide
+            }
+        }
+    }
+
 
     companion object {
         fun from(parent: ViewGroup): CardViewHolder {
@@ -47,10 +80,4 @@ class CardViewHolder private constructor(val binding: CardListItemBinding) :
         }
     }
 
-}
-
-class CardClickListener(val clickListener: (card: Card) -> Unit) {
-    fun onClick(card: Card) {
-        clickListener(card)
-    }
 }
