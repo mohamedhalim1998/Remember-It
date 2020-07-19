139866e7d7c95cb5b62bab593068f7bc0f31c534
MohamedHalim <mohamedhalim1998@gmail.com>
2020-07-19 23:31:42 +0200

    remove unused data converters

--- app/src/main/java/com/mohamed/halim/essa/flashcards/data/CardsDatabase.kt
+++ app/src/main/java/com/mohamed/halim/essa/flashcards/data/CardsDatabase.kt
@@ -4,13 +4,10 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
-import androidx.room.TypeConverters
 import com.mohamed.halim.essa.flashcards.data.model.Card
 import com.mohamed.halim.essa.flashcards.data.model.CardSet
-import com.mohamed.halim.essa.flashcards.util.RoomConverters
 
 @Database(entities = [CardSet::class, Card::class], version = 3)
-@TypeConverters(RoomConverters::class)
 abstract class CardsDatabase() : RoomDatabase() {
     abstract val cardsDao: CardsDao
 
