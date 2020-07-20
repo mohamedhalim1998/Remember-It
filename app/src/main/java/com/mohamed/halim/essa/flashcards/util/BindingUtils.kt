477b8252036b77851681ab953096c06d8b66003a
MohamedHalim <mohamedhalim1998@gmail.com>
2020-07-20 23:07:39 +0200

    Edit: display the max score

--- app/src/main/java/com/mohamed/halim/essa/flashcards/util/BindingUtils.kt
+++ app/src/main/java/com/mohamed/halim/essa/flashcards/util/BindingUtils.kt
@@ -2,8 +2,16 @@ package com.mohamed.halim.essa.flashcards.util
 
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
+import com.mohamed.halim.essa.flashcards.R
 
 @BindingAdapter("numberText")
-fun numberText(textView: TextView, num : Int) {
-    textView.text  = num.toString()
+fun numberText(textView: TextView, num: Int) {
+    textView.text = num.toString()
+}
+
+@BindingAdapter(value = ["score", "maxScore"])
+fun scoreText(textView: TextView, num: Int, max: Int) {
+    textView.apply {
+        text = context.getString(R.string.score_text, num, max)
+    }
 }
